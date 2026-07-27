a8b21aa94bfaabdd54e17b143e5986846a826e47
Jules Hémery <92369131+Couvbat@users.noreply.github.com>
2026-07-27 17:48:56 +0200

    Delete backend/dist directory

--- backend/dist/contact/contact.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ContactService } from './contact.service';
-import { ContactDto } from './contact.dto';
-export declare class ContactController {
-    private readonly contactService;
-    constructor(contactService: ContactService);
-    send(dto: ContactDto): Promise<{
-        ok: boolean;
-    }>;
-}
--- backend/dist/contact/contact.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { ContactDto } from './contact.dto';
-export declare class ContactService {
-    private config;
-    private readonly logger;
-    constructor(config: ConfigService);
-    send(dto: ContactDto): Promise<void>;
-}
